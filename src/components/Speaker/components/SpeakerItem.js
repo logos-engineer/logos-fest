f78f48c4298961b06e31376a7f11ff40bffedc08
Aqshola <aqsholafid2@gmail.com>
2021-09-02 20:35:19 +0700

    link speakers to speaker detail

--- src/components/Speaker/components/SpeakerItem.js
+++ src/components/Speaker/components/SpeakerItem.js
@@ -1,28 +1,30 @@
-import Image from 'next/image';
-import React from 'react';
-import SpeakerImg from '@/public//img/speaker/person-1.png';
-import useDataUrl from '@/hooks/useDataUrl';
+import Image from 'next/image'
+import React from 'react'
+import useDataUrl from '@/hooks/useDataUrl'
+import Link from 'next/link'
 const SpeakerItem = ({ name, imgSrc, university }) => {
-  const dataUrl = useDataUrl(275, 332);
+  const dataUrl = useDataUrl(275, 332)
   return (
-    <div className="relative w-full h-full rounded-2xl overflow-hidden">
-      <Image
-        src={imgSrc}
-        alt="Speaker Logos"
-        width={275}
-        height={332}
-        layout="responsive"
-        placeholder="blur"
-        blurDataURL={dataUrl}
-      />
-      <div className="bg-speaker-gradient absolute bottom-0 left-0 right-0 top-0">
-        <div className="absolute bottom-0 left-0 right-0 flex flex-col p-4 h-auto">
-          <h4 className="text-white text-md font-semibold">{name}</h4>
-          <p className="text-white text-base">{university}</p>
+    <Link href={`speakers/${name}`}>
+      <div className="relative w-full h-full rounded-2xl cursor-pointer overflow-hidden">
+        <Image
+          src={imgSrc}
+          alt="Speaker Logos"
+          width={275}
+          height={332}
+          layout="responsive"
+          placeholder="blur"
+          blurDataURL={dataUrl}
+        />
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-speaker-gradient">
+          <div className="absolute bottom-0 left-0 right-0 flex flex-col p-4 h-auto">
+            <h4 className="text-white text-md font-semibold">{name}</h4>
+            <p className="text-white text-base">{university}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </Link>
+  )
+}
 
-export default SpeakerItem;
+export default SpeakerItem
