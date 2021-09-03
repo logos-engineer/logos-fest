7643549d49e67193a858e5ce05d92858daf656fe
Aqshola <aqsholafid2@gmail.com>
2021-09-03 19:54:37 +0700

    finishing layout

--- src/pages/schedule/[event].js
+++ src/pages/schedule/[event].js
@@ -2,12 +2,15 @@ import React from 'react'
 import Layout from '@/components/Layout/Layout'
 import Grid from '@/components/Common/Grid'
 import SpeakerCard from '@/components/ScheduleDetail/SpeakerCard'
+import { Button } from '@/components/Common'
+import CardSchedule from '@/components/Homepage/Common/CardSchedule'
+import listSchedule from '@/data/listSchedule'
 
 export default function ScheduleDetail() {
   return (
     <Layout>
-      <Grid className="mt-32">
-        <div className="block col-span-7 col-start-1 pl-7 font-semibold">
+      <Grid className="mt-32 mx-auto max-w-screen-xl">
+        <div className="block col-span-7 col-start-1 font-semibold">
           <h1 className="mb-6 text-black-primary text-3xl">
             Concept of Justice
           </h1>
@@ -15,7 +18,7 @@ export default function ScheduleDetail() {
             <SpeakerCard />
             <SpeakerCard />
           </div>
-          <p className="text-black-primary text-opacity-75">
+          <p className="text-black-primary text-opacity-75 font-normal">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique
             tempor in dictumst sed mattis sed tellus. Aliquam, facilisi ante in
             neque blandit scelerisque nunc. Scelerisque risus quis mauris risus,
@@ -35,12 +38,38 @@ export default function ScheduleDetail() {
             tristique. Eu condimentum vivamus eleifend netus facilisi. Metus.
           </p>
         </div>
-        <div className="block col-span-3 col-start-9 h-96 bg-green-400"></div>
+        <div className="h-[max-content] block col-span-4 col-start-9 p-8 border rounded-xl">
+          <h1 className="mb-2 text-black-primary text-2xl font-semibold">
+            Date and Time
+          </h1>
+          <p className="mb-4 w-56 text-black-primary text-opacity-75">
+            Senin, 7 November 2021 17.00 - 18.00 WIB | 60 Menit
+          </p>
+          <button className="flex items-center mb-8 text-pink-500">
+            Add to Calendar
+            <img src="/icon/add.svg" alt="tambah" className="ml-2 w-4 h-4" />
+          </button>
+          <Button>Register Now</Button>
+        </div>
       </Grid>
-      <Grid className="mt-16">
-        <h1 className="col-span-12 pl-7 text-black-primary text-2xl font-semibold">
+      <Grid className="mt-16 mx-auto max-w-screen-xl">
+        <h1 className="col-span-12 text-black-primary text-2xl font-semibold">
           Kelas lainnya dari Bootcamp ini
         </h1>
+        <div className="grid gap-10 col-span-12 grid-cols-2">
+          {listSchedule[0].schedule.map((list) => (
+            <div className="col-span-1">
+              <CardSchedule
+                key={list.title}
+                title={list.title}
+                date={list.date}
+                subtitle={list.subtitle}
+                urlRegister={list.urlSchedule}
+                imgPerson={list.person}
+              />
+            </div>
+          ))}
+        </div>
       </Grid>
     </Layout>
   )
