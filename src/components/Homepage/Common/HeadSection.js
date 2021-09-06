9506d861829ce03e83ec5b836d7a02211f3bd897
Aqshola <aqsholafid2@gmail.com>
2021-09-06 13:50:16 +0700

    add responsive event

--- src/components/Homepage/Common/HeadSection.js
+++ src/components/Homepage/Common/HeadSection.js
@@ -1,24 +1,24 @@
-import clsx from 'clsx';
-import React from 'react';
-import PropTypes from 'prop-types';
+import clsx from 'clsx'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const HeadSection = ({ className, title, subtitle = '' }) => {
   return (
     <div className={clsx('flex flex-col ', className)}>
-      <h3 className="text-center text-black-primary text-3xl font-semibold leading-1.4 capitalize">
+      <h3 className="text-center text-black-primary text-lg font-semibold leading-1.4 capitalize lg:text-3xl">
         {title}
       </h3>
       {subtitle && subtitle.length > 0 && (
-        <h6 className="mt-4 text-center text-md">{subtitle}</h6>
+        <h6 className="mt-4 text-center text-sm lg:text-md">{subtitle}</h6>
       )}
     </div>
-  );
-};
+  )
+}
 
 HeadSection.propTypes = {
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string,
-};
+}
 
-export default HeadSection;
+export default HeadSection
