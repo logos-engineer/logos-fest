69ad45f0bbeb534c42c692f45d73eeb771d79408
naufaldi <naufaldi.rafif@gmail.com>
2021-09-08 06:08:23 +0700

    add sepaker responsive

--- src/components/Speaker/Hero.js
+++ src/components/Speaker/Hero.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Container, Grid } from '../Common';
+import React from 'react'
+import { Container, Grid } from '../Common'
 
 const Hero = () => {
   return (
     <Container>
       <Grid>
-        <div className="flex flex-col col-span-6 col-start-4 items-center mb-12">
-          <h3 className="text-center text-black-primary text-3xl leading-normal">
+        <div className="flex flex-col col-span-full items-center mb-12 sm:col-span-8 sm:col-start-3 lg:col-span-6 lg:col-start-4">
+          <h3 className="text-center text-black-primary text-lg font-semibold leading-normal sm:text-2xl lg:text-3xl">
             Speakers
           </h3>
-          <p className="mt-4 text-center text-black-body text-lg leading-normal">
+          <p className="mt-4 text-center text-black-body text-base leading-normal sm:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa cras
             ipsum dolor sit amet, consectetur{' '}
           </p>
         </div>
       </Grid>
     </Container>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
--- src/pages/speakers/index.js
+++ src/pages/speakers/index.js
@@ -1,17 +1,17 @@
-import Layout from '@/components/Layout/Layout';
-import Hero from '@/components/Speaker/Hero';
-import ListSpeaker from '@/components/Speaker/ListSpeaker';
-import React from 'react';
+import Layout from '@/components/Layout/Layout'
+import Hero from '@/components/Speaker/Hero'
+import ListSpeaker from '@/components/Speaker/ListSpeaker'
+import React from 'react'
 
 const Speakers = () => {
   return (
     <Layout>
-      <section id="speaker-logos" className="pt-120px w-full">
+      <section id="speaker-logos" className="pt-120px px-4 w-full lg:px-0">
         <Hero />
         <ListSpeaker />
       </section>
     </Layout>
-  );
-};
+  )
+}
 
-export default Speakers;
+export default Speakers
