c235c73c3776541529bde969cf3efafa599bafbe
Sintha Sinurat <sinthadf@gmail.com>
2021-09-14 18:17:07 +0700

    add variabels events and speakers

---
+++ src/data/variabels/events.js
@@ -0,0 +1,155 @@
+import {
+  speaker1,
+  speaker2,
+  speaker3,
+  speaker4,
+  speaker5,
+  speaker6,
+  speaker7,
+  speaker8,
+  speaker9,
+  speaker10,
+  speaker11,
+  speaker12,
+  speaker13,
+} from './speakers'
+
+const philosophy1 = {
+  id: 1,
+  title: 'Concept of Justice',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Rabu, 3 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker1,
+  slug: 'concept-ofjustice',
+}
+const philosophy2 = {
+  id: 2,
+  title: 'Justice in Pre-Modern Era',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Rabu, 3 November 2021',
+  date: 'Jumat, 5 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker2,
+  slug: 'justice-in-pre-modern-era',
+}
+const philosophy3 = {
+  id: 3,
+  title: 'Justice in Modern Era',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Sabtu, 6 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker3,
+  slug: 'justice-in-modern-era',
+}
+const philosophy4 = {
+  id: 4,
+  title: 'Contemporary Justice',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Minggu, 7 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker4,
+  slug: 'contemporary-justice"',
+}
+
+const law1 = {
+  id: 5,
+  title: 'Equity in Law',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Jumat, 10 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker5,
+  slug: 'equity-in-law',
+}
+const law2 = {
+  id: 6,
+  title: 'Restorative Justice',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Minggu, 12 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker6,
+  slug: 'restorative-justice',
+}
+const law3 = {
+  id: 7,
+  title: 'Cyber Law',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Selasa, 14 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker7,
+  slug: 'cyber-law',
+}
+
+const politics1 = {
+  id: 8,
+  title: 'Sejarah dan Aliran Politik Indonesia',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Jumat, 17 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker8,
+  slug: 'sejarah-dan-aliran-politik-indonesia',
+}
+const politics2 = {
+  id: 9,
+  title: 'Politik Dinasti Indonesia',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Minggu, 19 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker9,
+  slug: 'politik-dinasti-indonesia',
+}
+const politics3 = {
+  id: 10,
+  title: 'Youth Political Participation in Indonesia',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Selasa, 21 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker10,
+  slug: 'youth-political-participation-in-indonesia',
+}
+
+const gender1 = {
+  id: 11,
+  title: 'Gender dan Feminisme',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Jumat, 24 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker11,
+  slug: 'gender-dan-feminisme',
+}
+const gender2 = {
+  id: 12,
+  title: 'Keadilan dan Kesetaraan Gender dalam Seni',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Minggu, 26 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker12,
+  slug: 'keadilan-dan-kesetaraan-gender-dalam-seni',
+}
+const gender3 = {
+  id: 13,
+  title: 'Digital dan Gender',
+  subtitle:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora ab quibusdam magni rerum dolorem distinctio at odit similique, sed repudiandae praesentium molestiae. Autem quibusdam, assumenda cum eum corporis maiores!',
+  date: 'Selasa, 28 November 2021',
+  time: '19.00 - 21.00',
+  speaker: speaker13,
+  slug: 'digital-dan-gender',
+}
+
+export const kelas1 = [philosophy1, philosophy2, philosophy3, philosophy4]
+export const kelas2 = [law1, law2, law3]
+export const kelas3 = [politics1, politics2, politics3]
+export const kelas4 = [gender1, gender2, gender3]
---
+++ src/data/variabels/speakers.js
@@ -0,0 +1,104 @@
+export const speaker1 = {
+  id: 1,
+  name: 'Dr. Phil Fitzerald Kennedy Sitorus, S.Sos., S.Fil.',
+  jabatan: 'Dosen Universitas Pelita Harapan',
+  profile:
+    'Dr. Fitzerald Kennedy Sitorus adalah Dosen Program Studi Liberal Arts. Ia mengajar mata kuliah Sejarah Pemikiran, Filsafat Ilmu Pengetahuan, Etika, dan Logika Terapan. Bidang minatnya meliputi membaca, menulis, dan diskusi. Beliau memperoleh gelar Sarjana Filsafat dari Sekolah Tinggi Filsafat Driyarkara, dan Doktor der Philosophie dari Johann Wolfgang Goethe-Universität, Frankfurt am Main di Jerman',
+  img: '/img/speaker/phil-fitzerald.jpeg',
+}
+export const speaker2 = {
+  id: 2,
+  name: 'Rm. Prof. Dr. Franz Magnis Suseno, SJ',
+  jabatan: 'Guru Besar Emeritus Sekolah Tinggi Filsafat Driyarkara',
+  profile:
+    'Romo Prof. Dr. Franz Magnis-Suseno SJ, rohaniwan, lahir 1936 di Jerman, sejak 1961 di Indonesia, sejak 1977 WNI, belajar filsafat, teologi dan teori politik di Pullach, Yogyakarta dan München; doktorat dalam filsafat 1973 dari Universitas München, sejak 1969 dosen tetap dan sekarang guru besar emeritus Sekolah Tinggi Filsafat Driyarkara di Jakarta. 1987-1998 Ketua Sekolah Tinggi Filsafat Driyarkara, 2008-2017 Ketua Pengurus Yayasan Pendidikan Driyarkara. Sejak 1976 ia mengajar di Universitas Indonesia dan selama 9 tahun di Universitas Katolik Parahyangan Bandung. Ia juga menjadi dosen tamu di beberapa perguruan tinggi di Eropa. ',
+  img: '/img/speaker/franz-magnis.jpg',
+}
+export const speaker3 = {
+  id: 3,
+  name: 'Drs. Agus Wahyudi, M.Si., M.A., Ph.D',
+  jabatan: 'Dosen Filsafat Politik, Fakultas Filsafat UGM',
+  profile:
+    '2009-2018 Ph.D, Northern Arizona University, USA,2002-2003 M.A. International School for Humanities and Social Sciences (ISHSS), University of Amsterdam, The Netherlands, 1998-1999 M.A. Research School of Pacific and Asian Studies (RSPAS), Australian National University, Australia (Incomplete), 1993-1997 MSi (Master of Science), Gadjah Mada University, Yogyakarta 1987-1992 Bachelor, Philosophy, Gadjah Mada University, Yogyakarta',
+  img: '/img/speaker/agus-wahyudi.png',
+}
+export const speaker4 = {
+  id: 4,
+  name: 'Martin Suryajaya, M.Hum',
+  jabatan: 'Kandidat Doktor STF Driyarkara, Jakarta',
+  profile:
+    '2005-2009 S1 STF Driyarkara, Jakarta (jurusan Filsafat) 2010-2012 S2 STF Driyarkara, Jakarta (jurusan Filsafat), 2019 - sekarang kandidat doktor STF Driyarkara, Jakarta (jurusan Filsafat)',
+  img: '/img/speaker/martin-suryajaya.jpg',
+}
+export const speaker5 = {
+  id: 5,
+  name: 'Zainal Arifin Mochtar',
+  jabatan: 'Pakar Hukum Tata Negara UGM',
+  profile:
+    'Zainal Arifin Mochtar adalah seorang dosen Fakultas Hukum Universitas Gadjah Mada (FH-UGM) Yogyakarta. Zainal juga merupakan alumnus dari tempat ia mengajar. Lulus bergelar sarjana hukum dari UGM di tahun 2003 dengan menyusun tugas akhir berjudul “Konsep Pertanggungjawaban Pelaku Crimes Against Humanity di Pengadilan HAM. Aktif sebagai dosen, juga pegiat antikorupsi dirinya juga aktif meneliti. Zainal tercatat pernah tergabung dalam Pusat Studi Hak Asasi Manusia (PUSHAM) UII Yogyakarta tahun 2001. Sebagai peneliti, Zainal memiliki ketertarikan pada Peradilan Tata Usaha Negara, Hukum Administrasi Negara dan Sosio Humaniora. Di tahun 2012 dirinya memeroleh gelar doktoral dari Universitas Gadjah Mada (UGM) Yogakarta.',
+  img: '/img/speaker/zainal-arifin.jpg',
+}
+export const speaker6 = {
+  id: 6,
+  name: 'Gandjar Laksmana Bonaprapta',
+  jabatan: 'Pakar Hukum Universitas Indonesia',
+  profile:
+    'Ganjar Laksmana Bonaprapta merupakan ahli hukum pidana dan pengajar pada Fakultas Hukum Universitas Indonesia. ',
+  img: '/img/speaker/gandjar-laksmana.png',
+}
+export const speaker7 = {
+  id: 7,
+  name: 'Dr. Tasya Safiranita, SH., MH.',
+  jabatan: 'Kepala Pusat Studi Cyber Law & Transformasi Digital FH Unpad',
+  profile:
+    'Dr. Tasya Safiranita, SH., MH. is a permanent lecturer at the Faculty of Law, Padjajaran University. In 2008 she received her Bachelor of Law degree from the Faculty of Law, Padjajaran University. Subsequently, she earned a Masters in Business Law and a Doctor of Law from the Faculty of Law, Padjajaran University, with cum laude.',
+  img: '/img/speaker/tasya-safiranita.jpeg',
+}
+export const speaker8 = {
+  id: 8,
+  name: 'Peter Carey',
+  jabatan: 'Sejarawan, Dosen Sejarah Universitas Indonesia',
+  profile:
+    'Fellow Emeritus (profesor penghormatan) dari Trinity College, Oxford. Tutor sejarah modern di Trinity College, Oxford (1979-2008). Adjunct Professor (Profesor Tamu), Fakultas Ilmu Budaya Universitas Indonesia',
+  img: '/img/speaker/peter-carey.jpeg',
+}
+export const speaker9 = {
+  id: 9,
+  name: 'Yoes C. Kenawas',
+  jabatan: 'Peneliti Ilmu Politik',
+  profile:
+    'MA in Political Science, Northwestern University (Amerika Serikat)M.Sc in Asian Studies, S. Rajaratnam School of International Studies, Nanyang Technological University (Singapura)M.Si in Corporate Communications, London School of Public Relations (Indonesia)S.IP Hubungan Internasional - Universitas Katolik Parahyangan (Indonesia)',
+  img: '/img/speaker/yoes-kenawas.jpeg',
+}
+export const speaker10 = {
+  id: 10,
+  name: 'Amalia Nur Andini S.Hum., M.A.',
+  jabatan: 'Peneliti Ilmu Politik',
+  profile:
+    'MA in Political Science, Northwestern University (Amerika Serikat)M.Sc in Asian Studies, S. Rajaratnam School of International Studies, Nanyang Technological University (Singapura)M.Si in Corporate Communications, London School of Public Relations (Indonesia) S.IP Hubungan Internasional - Universitas Katolik Parahyangan (Indonesia)',
+  img: '/img/speaker/amalia-nur-andini.jpg',
+}
+export const speaker11 = {
+  id: 11,
+  name: 'Ikhaputri Widiantini',
+  jabatan: 'Dosen Ilmu Filsafat Universitas Indonesia',
+  profile:
+    'Mengajar di Program Sarjana Ilmu Filsafat, Fakultas Ilmu Pengetahuan Budaya, Universitas Indonesia. Saat ini di UI mengampu kelas Paradigma Feminis, Filsafat dan HAM, Metafisika, Filsafat Seni, dan Estetika. Pernah memegang project pendidikan di Yayasan Jurnal Perempuan, menjadi redaksi Jurnal Perempuan periode 2009-2011, dan hingga sekarang masih aktif membantu sebagai editor lepas di Jurnal Perempuan. Memiliki ketertarikan pada riset bertema feminisme, seni, dan estetika terutama pada pemikiran abjeksi dari Julia Kristeva. ',
+  img: '/img/speaker/ikhaputri-widiantini.jpeg',
+}
+export const speaker12 = {
+  id: 12,
+  name: 'Syarif Maulana',
+  jabatan: 'Dosen Filsafat Universitas Parahyangan',
+  profile:
+    'Syarif Maulana lahir di Bandung, 30 November 1985. Saat ini ia menjadi pengajar di Fakultas Filsafat, Universitas Katolik Parahyangan dan mahasiswa doktoral di Sekolah Tinggi Filsafat Driyarkara. Di masa pandemi, Syarif menginisiasi kelas belajar filsafat daring bernama Kelas Isolasi. Buku yang pernah ditulisnya antara lain Kumpulan Kalimat Demotivasi: Panduan Menjalani Hidup dengan Biasa-Biasa Saja (2020), Nasib Manusia: Kisah Awal Uzhara, Eksil di Rusia (2021) dan Kumpulan Kalimat Demotivasi 2: Panduan Hidup Bahagia untuk Medioker (2021). Syarif juga merupakan ketua panitia Philofest ID 2020 dan pengajar PhiloKids atau kelas filsafat untuk anak usia 10-15 tahun. ',
+  img: '/img/speaker/syarif-maulana.jpg',
+}
+export const speaker13 = {
+  id: 13,
+  name: 'Hera Diani',
+  jabatan: 'Writer/Editor/Communication Consultant',
+  profile:
+    'An accomplished Journalist with  21 years of experience for print anddigital media. A Co-founder of Magdalene.co, a web magazine focusingon women and gender issues. A seasoned Communication Consultantspecializing in sustainable development and public health campaign,for non-profits and multilateral agencies',
+  img: '/img/speaker/hera-diani.png',
+}
