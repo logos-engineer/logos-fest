dbe597a6662d2cd4a6b18c6067caee3242e9f0ac
Aqshola <aqsholafid2@gmail.com>
2021-09-14 20:22:44 +0700

    add notfound mobile

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,18 +15,18 @@ export default function Custom404() {
         <title>Halaman tidak ditemukan</title>
       </Head>
       <Layout>
-        <Container className="relative flex-col justify-center pl-16 h-screen">
+        <Container className="relative flex-col justify-center px-4 h-screen lg:pl-16">
           <h1 className="mb-4 text-black-primary text-3xl font-semibold">
             Halaman tidak ditemukan{' '}
           </h1>
-          <p className="w-[480px] mb-12">
+          <p className="lg:w-[480px] mb-12 w-full">
             Tampaknya halaman yang kamu kunjungi tidak tersedia untuk saat ini
           </p>
-          <Button className="w-max" onClick={() => route.push('/')}>
+          <Button className="w-full lg:w-max" onClick={() => route.push('/')}>
             Kembali ke Home
           </Button>
         </Container>
-        <div className="w-[459] h-[460] pb-[24px] absolute -bottom-40 right-0 self-end">
+        <div className="w-[230px] h-[230px] pb-[24px] lg:w-[459] lg:h-[460] absolute -bottom-32 right-0 self-end lg:-bottom-40">
           <Image
             src="/img/404/illust.png"
             width={459}
