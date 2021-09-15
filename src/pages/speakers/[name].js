c46b851d15b82e36052987aa9a313e68f14787d1
Aqshola <aqsholafid2@gmail.com>
2021-09-15 14:20:41 +0700

    add speaker info hardcode

--- src/pages/speakers/[name].js
+++ src/pages/speakers/[name].js
@@ -4,24 +4,44 @@ import listSchedule from '@/data/listSchedule'
 import SpeakerInfo from '@/components/SpeakerDetail/SpeakerInfo'
 import SpeakerClass from '@/components/SpeakerDetail/SpeakerClass'
 import Head from 'next/head'
+import ListSpeakerName from '@/data/listSpeakerName'
 
-export default function SpeakerDetail() {
+export default function SpeakerDetail({ speakerData }) {
+  console.log(speakerData)
   return (
     <>
       <Head>
         <title>Speaker Detail</title>
       </Head>
       <Layout>
-        /*
-         * TODO : use getServerSide/getStaticProps to get data for detail speaker from listSpeakerName using id/slug
-         */
+        /* * TODO : use getServerSide/getStaticProps to get data for detail
+        speaker from listSpeakerName using id/slug */
         <SpeakerInfo
-          photo="/img/speaker/person-2.png"
-          name="Zildjidan Ramadhan Syam"
-          univ="Head of Diversity, Inclusion & Belonging "
+          photo={speakerData.img}
+          name={speakerData.name}
+          univ={speakerData.jabatan}
+          desc={speakerData.profile}
         />
-        <SpeakerClass listClass={listSchedule[0].schedule} />
+        {/* <SpeakerClass listClass={listSchedule[0].schedule} /> */}
       </Layout>
     </>
   )
+}
+export function getStaticPaths() {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: 'blocking', //indicates the type of fallback
+  }
+}
+
+export function getStaticProps({ params }) {
+  const speakerIndex = ListSpeakerName.map((data) => data.slug).indexOf(
+    params.name
+  )
+  const speakerData = ListSpeakerName[speakerIndex]
+  return {
+    props: {
+      speakerData,
+    },
+  }
 }
