576e25d45f3503bd2b005c35b748cb2d6d608c53
Sintha Sinurat <sinthadf@gmail.com>
2021-09-28 20:16:35 +0700

    add seo component and favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,12 @@ import Event from '@/components/Homepage/Event'
 import Schedule from '@/components/Homepage/Schedule'
 import Book from '@/components/Homepage/Book'
 import Sponsors from '@/components/Homepage/Sponsors'
+import Seo from '@/components/Seo'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Logos Festival</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Seo templateTitle="Logos Festival" />
       <Layout>
         <Hero />
         <Event />
